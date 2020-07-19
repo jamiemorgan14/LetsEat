4b379f6b71cabc75d5520fb89f7e074913c7ef21
Jamie Morgan <jamiemorgan@truckstop.com>
2020-07-19 01:29:41 -0600

    Stuff

---
+++ Let'sEat/Explore/ExploreViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ExploreViewController.swift
+//  Let'sEat
+//
+//  Created by Jamie Morgan on 7/18/20.
+//  Copyright © 2020 Jamie Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+        return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath)
+        return cell
+    }
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindLocationCancel(segue: UIStoryboardSegue) {
+        
+    }
+ }
