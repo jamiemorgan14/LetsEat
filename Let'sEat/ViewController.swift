2ee7a2c705f54bf75e0405fd6cfbf38bacb63259
Jamie Morgan <jamiemorgan@truckstop.com>
2020-07-18 21:58:26 -0600

    Initial Commit

---
+++ Let'sEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Let'sEat
+//
+//  Created by Jamie Morgan on 7/18/20.
+//  Copyright © 2020 Jamie Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
