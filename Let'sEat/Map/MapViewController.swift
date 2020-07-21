cf3533adb734f3e0ba6c15a19cb5f79191ed057e
Jamie <jmorgan3475@gmail.com>
2020-07-21 14:59:18 -0600

    connect map scene to mapViewController

---
+++ Let'sEat/Map/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  Let'sEat
+//
+//  Created by Jamie Morgan on 7/21/20.
+//  Copyright © 2020 Jamie Morgan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Let'sEat/Map/Model/MapDataManager.swift
+++ Let'sEat/Map/Model/MapDataManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import MapKit
 
 class MapDataManager: DataManager {
     fileprivate var items: [RestaurantItem] = []
@@ -23,5 +24,15 @@ class MapDataManager: DataManager {
             items.append(RestaurantItem(dict: data))
         }
         completion(items)
+    }
+    
+    func currentRegion(latDelta: CLLocationDegrees, longDelta: CLLocationDegrees) -> MKCoordinateRegion {
+        guard let item = items.first else {
+            return MKCoordinateRegion()
+        }
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        return MKCoordinateRegion(center: item.coordinate, span: span)
     }
 }
