77a34f3bb04c86734fc01e250b78acc2eef73106
Jamie <jmorgan3475@gmail.com>
2020-07-23 20:55:03 -0600

    added json files

---
+++ Let'sEat/Misc/RestaurantAPIManager.swift
@@ -0,0 +1,31 @@
+//
+//  RestaurantAPIManager.swift
+//  Let'sEat
+//
+//  Created by Jamie Morgan on 7/23/20.
+//  Copyright © 2020 Jamie Morgan. All rights reserved.
+//
+
+import Foundation
+
+struct RestaurantAPIManager {
+    static func loadJSON(file name: String) -> [[String: AnyObject]] {
+        var items = [[String: AnyObject]]()
+        guard let path = Bundle.main.path(forResource: name, ofType: "json"),
+            let data = NSData(contentsOfFile: path) else {
+            return [[:]]
+        }
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data as Data, options: .allowFragments) as AnyObject
+            if let restaurants = json as? [[String: AnyObject]] {
+                items = restaurants as [[String: AnyObject]]
+            }
+        }
+        catch {
+            print("error serializing JSON: \(error)")
+            items = [[:]]
+        }
+        return items
+    }
+}
