439930a3e570bdc6a53a2bca88cd87c2194ed65c
Jamie <jmorgan3475@gmail.com>
2020-07-23 23:19:00 -0600

    Added ExploreHeaderView

---
+++ Let'sEat/Location/View/ExploreHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  ExploreHeaderViewCollectionReusableView.swift
+//  Let'sEat
+//
+//  Created by Jamie Morgan on 7/23/20.
+//  Copyright © 2020 Jamie Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ExploreHeaderView: UICollectionReusableView {
+        
+}
