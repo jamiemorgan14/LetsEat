d8392a447ffacb6a7bf65527b5ab98fbaf1baf40
Jamie <jmorgan3475@gmail.com>
2020-07-28 17:23:43 -0600

    create no data page

---
+++ Let'sEat/Misc/No Data/NoDataView.swift
@@ -0,0 +1,46 @@
+//
+//  NoDataView.swift
+//  Let'sEat
+//
+//  Created by Jamie Morgan on 7/28/20.
+//  Copyright © 2020 Jamie Morgan. All rights reserved.
+//
+
+import UIKit
+
+class NoDataView: UIView {
+    var view: UIView!
+    @IBOutlet var lblTitle: UILabel!
+    @IBOutlet var lblDesc: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        setupView()
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let nib = UINib(nibName: "NoDataView", bundle: Bundle.main)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        return view
+    }
+    
+    func setupView() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+    }
+    
+    func set(title: String) {
+        lblTitle.text = title
+    }
+    
+    func set(desc: String) {
+        lblDesc.text = desc
+    }
+}
