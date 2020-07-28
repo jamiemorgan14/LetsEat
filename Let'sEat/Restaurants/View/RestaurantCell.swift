a1bec742bff7d4643983028cc83ed44dd226e346
Jamie <jmorgan3475@gmail.com>
2020-07-27 23:08:07 -0600

    Create restaurantCell UICollectionViewCell

---
+++ Let'sEat/Restaurants/View/RestaurantCell.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantCell.swift
+//  Let'sEat
+//
+//  Created by Jamie Morgan on 7/27/20.
+//  Copyright © 2020 Jamie Morgan. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UICollectionViewCell {
+    
+}
