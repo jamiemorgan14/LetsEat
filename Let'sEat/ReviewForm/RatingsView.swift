5aa624144cc5afe959fd968cef4c48a4b85a530e
Jamie <jmorgan3475@gmail.com>
2020-07-31 21:59:44 -0600

    Add ratings view class

---
+++ Let'sEat/ReviewForm/RatingsView.swift
@@ -0,0 +1,13 @@
+//
+//  RatingsView.swift
+//  Let'sEat
+//
+//  Created by Jamie Morgan on 7/31/20.
+//  Copyright © 2020 Jamie Morgan. All rights reserved.
+//
+
+import UIKit
+
+class RatingsView: UIControl {
+    
+}
